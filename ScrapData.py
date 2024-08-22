869302c6f5509efba3524e0d357cf5af3a808cfe
shankarmurugesan <37131658+shankarmurugesan@users.noreply.github.com>
2024-08-22 17:14:56 +0530

    ScrapData.py

--- ScrapData.py
+++ ScrapData.py
@@ -4,6 +4,7 @@ import pandas as pd
 from datetime import datetime, timedelta
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.action_chains import ActionChains
